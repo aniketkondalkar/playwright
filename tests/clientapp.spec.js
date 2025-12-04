76d806c0b98a60adfa40670c58f77d6428a09a9c
aniket <aniketkondalkar@gmail.com>
2025-12-05 00:13:00 +0530

    checkout page

--- tests/clientapp.spec.js
+++ tests/clientapp.spec.js
@@ -4,7 +4,7 @@ test('Search Product dynamically',async ({page})=>
 {
 const productName = "ADIDAS ORIGINAL";
 const products =page.locator(".card-body");
-await page.goto("https://rahulshettyacademy.com/client");
+await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
 await page.locator("[type='email']").fill("kondalkar@gmail.com");
 await page.locator("[type='password']").fill("Aniket@123");
 await page.locator("input#login").click();
@@ -27,6 +27,26 @@ await page.locator("[routerlink*='cart']").click();
 await page.locator("div li").first().waitFor();
 const bool = await page.locator("h3:has-text('ADIDAS ORIGINAL')").isVisible();
 expect(bool).toBeTruthy();
+await page.locator("button[type='button']").last().click();
+const cvv= page.locator("div[class='field small']").nth(1);
+await cvv.locator("input").fill("345");
+//await page.pause();
+ const NameOnCard = page.locator("div[class='field']").last();
+ await NameOnCard.locator("input").fill("Aniket Kondalkar");
+ await page.locator("input[placeholder='Select Country']").pressSequentially("ind",{delay:150});
+ const countryDD = page.locator(".ta-results");
+ await countryDD.waitFor();
+ const countryCount=await countryDD.locator("button").count();
+for (let i=0; i<countryCount ; ++i)
+{
+ const text = await countryDD.locator("button").nth(i).textContent();
+ if (text === " India")
+ {
+    await countryDD.locator("button").nth(i).click();
+    break;
+ }
+}
+await page.pause();
 
 }
 )
